1128e30ec2919f3373d424e1c288436ffcab5b0f
Mitchell Morris <mitchell@morrisland.com>
2009-04-10 15:38:51 -0700

    Initial import

---
+++ SSODailies.lua
@@ -0,0 +1,44 @@
+
+TourGuide:RegisterGuide("Isle of Quel'Danas (SSO dailies)", nil, nil, function()
+return [[
+A Know Your Ley Lines |QID|11547| |N|From Astromancer Darnarian, on the ground floor of the building where you enter the island.|
+A The Air Strikes Must Continue |QID|11533| |N|From Battlemage Arynna|
+A The Battle Must Go On |QID|11537| |N|From Harbinger Inuuro|
+A Open for Business |QID|11546|
+A Further Conversions |QID|11525|
+A Arm the Wards! |QID|11523|
+A Keeping the Enemy at Bay |QID|11543|
+A Crush the Dawnblade |QID|11540|
+A Don't Stop Now.... |QID|11536| |N|Jump off the ledge into the courtyard below.|
+K Emissary of Hate. |Q|The Battle Must Go On|QO|Emissary of Hate impaled: 1/1| |U|34414| |N|Head west to the courtyard where the demons have gathered. The Emissary of Hate patrols in and out of the glowy portal. After the Emissary of Hate is dead, target his corpse and impale it with the flag. You should also be killing demons, and look for bloodberry bushes to pick as well.|
+N Take the first portal reading.| |Q|Know Your Ley Lines|QO|Portal Reading Taken: 1/1| |U|34533| |N|Walk along the eastern side of the line of the gantlet of demons to the portal, killing demons.|
+C The Battle Must Go On |QID|11537|
+N Take the bloodcrystal reading. |Q|Know Your Ley Lines|QO|Bloodcrystal Reading Taken: 1/1| |U|34533| |N|Ride northeast back to the armory, then northwest to Dawnstar Village (41.87, 35.44). Clear your way into the eastern door of the building. Kill everything until you get to the crystal, then take the reading.|
+C Crush the Dawnblade |QID|11540| |N|Clear your way south through the door, then around to the western side of the building. There will be knights on horseback and individual marksmen. There should also be some bloodberry bushes along the way.|
+C Further Conversions |QID|11525| |U|34368| |N|The sentries walk all around the area. Make your way north, then east, converting the sentries. Also keep killing the wretched undead until you have four mana remnants, and gather bloodberries from the bushes.|
+K Wretched undead. | |L|34338 4| |N|Until you have four mana remnants, and keep gathering bloodberries until you have five of them.|
+C Open for Business |QID|11546| |N|The bloodberry bushes grow all around here.|
+C The Air Strikes Must Continue |QID|11533| |U|34475| |N|Talk to Ayren Cloudbreaker at (48.40, 25.28) and tell him "Speaking of action, I've been ordered to undertake an air strike.". You'll be put on a dragon, and you'll fly all the way south, then back up the scar. Use the arcane missiles to kill the demons on the ground, and the sorcerers on the building ledges.|
+C Keeping the Enemy at Bay |QID|11543| |U|34489| |N|Talk to Ayren Cloudbreaker at (48.40, 25.28) again, and tell him "I need to intercept the Dawnblade reinforcements". You'll be on another dragon flying out to the ships. Set the ships' sails on fire while you're flying around, then kill six reservists once you land.|
+R Isle of Quel'Danas | |N|Talk to the unrestrained dragonhawk to get back to land.|
+C Arm the Wards! |QID|11523| |U|34338| |N|Stand next to one of the big mana crystals and use the mana remnants. There's one at (49.08, 29.30)|
+A Disrupt the Greengill Coast |QID|11541|
+R Greengill Coast | |N|Ride south to Sun's Reach Armory, then east to the coast.|
+C Disrupt the Greengill Coast |QID|11541| |U|34483| |N|Kill the Darkspine Sirens to loot Orbs of Murloc Control. Use the orb on the captive murlocs to free them. Also kill the Darkspine Myrmidons to loot the keys to the chests with the Darkspine Iron Ore.|
+C Don't Stop Now.... |QID|11536| |N|Keep killing Darkspine Myrmidons as you make your way south, looting the keys to the chests, and opening chests to get the ore until you have three ore samples.|
+N Take the shrine reading |Q|Know Your Ley Lines|QO|Shrine Reading Taken: 1/1| |U|34533| |N|South along the coastline at (47.50, 35.20)|
+T Disrupt the Greengill Coast |QID|11541|
+T Open for Business |QID|11546|
+T Further Conversions |QID|11525|
+T Arm the Wards! |QID|11523|
+T The Battle Must Go On |QID|11537|
+A The Battle Must Go On |QID|11537| |N|From Harbinger Inuuro|
+T The Air Strikes Must Continue |QID|11533|
+T Know Your Ley Lines |QID|11547|
+T Keeping the Enemy at Bay |QID|11543|
+T Crush the Dawnblade |QID|11540|
+T Don't Stop Now.... |QID|11536|
+N Repair, restock, vend junk.| |N|Smith Hauthaa will take care of the vending junk and repairing armor.|
+U Darnarian's Scroll of Teleportation|cffffffff|Hitem:35230:0:0:0:0:0:0:679544896:75|h[Darnarian's Scroll of Teleportation]|h|r |U|35230| |N|This ports you back to Shattrath|
+]]
+end)
